affae93557f9b3198aeaac94a05fdf7983a07057
choi-won-ik <whwwhs7837@gmail.com>
2025-02-26 14:46:53 +0900

    [Feat] 댓글 기능 추가

--- src/page/details.vue
+++ src/page/details.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { RouterLink, useRoute } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 import { ref, onMounted } from "vue";
 import { userBoardStore } from '../store/userBoardStore.js';
+
+import axios from 'axios';
 
 const store = userBoardStore();
 const board = ref('');
 const route = useRoute();
+const router = useRouter();
 const boadIdx = route.params.idx;
 
 onMounted(async () => {
@@ -14,6 +17,30 @@ onMounted(async () => {
     board.value = store.board;
     console.log(board.value);
 });
+
+
+
+// 데이터와 메서드 정의
+const form = ref({
+    boardIdx:boadIdx,
+  writer: '',
+  content: ''
+});
+
+
+// 폼 제출 처리 함수
+const handleSubmit = async () => {
+  try {
+    console.log(form);
+    // axios로 폼 데이터 제출
+    const response = await axios.post('/api/comment/regist', form.value);
+
+    // 성공 시 다른 URL로 리다이렉트
+    window.location.reload();
+  } catch (error) {
+    console.error('폼 제출 오류:', error);
+  }
+};
 </script>
 
 <template>
@@ -24,6 +51,16 @@ onMounted(async () => {
 
         <hr>
         <h2>댓글</h2>
+        <form @submit.prevent="handleSubmit">
+            <div>
+                작성자 : <input type="text" style="width: 350px;" name="writer"  v-model="form.writer" id="writer">
+            </div>
+            <div style="margin:15px 0px">
+                내용 : <input type="text" style="width: 400px;" name="content" v-model="form.content" id="content">
+            </div>
+            
+            <button type="submit">작성</button>
+        </form>
         <div v-for="comment in board.comments" :key="comment.idx">
             <div style="display: flex;">
                 <div style="margin-left: 100px;">작성자 : {{ comment.writer }}</div>
